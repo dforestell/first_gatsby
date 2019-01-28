4e1edf03f586c5e6a1f8a2eb243f7e4e732dbd97
Dillon Forestell <dillons-mac@Dillons-MacBook-Pro.local>
2019-01-28 11:41:37 -0600

    add a directory to the pages folder and create page three within

---
+++ src/pages/dir1/page-3.js
@@ -0,0 +1,7 @@
+import React from "react"
+import Link from "gatsby-link"
+
+export default () => <div style={{color: 'blue'}} >
+						<h1>Here is page 3</h1>
+						<Link to="/"> Home </Link> 
+					</div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import Link from "gatsby-link"
 
 export default () => <div>
-						<h1>Hello world!!!!!</h1>
-						<Link to="/page-2/"> Page 2 </Link> 
+						<h1>Hello world!</h1>
+						<Link to="/page-2/"> Page 2 </Link>
+						<Link to="/dir1/page-3/"> Page 3</Link> 
 					</div>
