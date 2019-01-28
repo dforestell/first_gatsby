5cc6ca91b8b8f86b9fd053306fdbb899134dda9d
Dillon Forestell <dillons-mac@Dillons-MacBook-Pro.local>
2019-01-28 11:43:29 -0600

    add a line break betweek links to pages 2 and 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,5 +4,6 @@ import Link from "gatsby-link"
 export default () => <div>
 						<h1>Hello world!</h1>
 						<Link to="/page-2/"> Page 2 </Link>
+						<br />
 						<Link to="/dir1/page-3/"> Page 3</Link> 
 					</div>
